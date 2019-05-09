91aa37618a2b773c2894d303c5b89620bae3edbc
My Ho <mydiemho@gmail.com>
2019-05-09 13:50:30 -0700

    clean up all unneed files

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 set -e
 
-export RESOURCE_GROUP="team12-rg"
-
+export RESOURCE_GROUP="team12-rg-2"
+export AKS_CLUSTER="team12-aks"
 echo "Creating resource group"
 az group create --name ${RESOURCE_GROUP} --location westus2
 
 echo "Creating AKS cluster"
 az aks create \
     --resource-group ${RESOURCE_GROUP} \
-    --name MyAKS \
+    --name ${AKS_CLUSTER} \
     --node-count 3 \
     --enable-addons monitoring \
     --generate-ssh-keys
 
-echo "Setting up dev spaces"
-az aks use-dev-spaces -g ${RESOURCE_GROUP} -n MyAKS
+az aks get-credentials --resource-group ${RESOURCE_GROUP} --name ${AKS_CLUSTER}   
