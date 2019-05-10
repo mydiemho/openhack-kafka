bc08efe5adc2d061a2d51f75697169e1859ded47
My Ho <mydiemho@gmail.com>
2019-05-09 17:11:05 -0700

    working producer with avro schema
    1. update pom.xml to support avro
    1. update producer code to add schema to schema registry

--- producer-app-java/src/main/java/com/microsoft/cse/kafkaopenhack/javaexamples/SimpleProducer.java
+++ producer-app-java/src/main/java/com/microsoft/cse/kafkaopenhack/javaexamples/SimpleProducer.java
@@ -1,5 +1,7 @@
 package com.microsoft.cse.kafkaopenhack.javaexamples;
 
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -11,17 +13,25 @@ import com.google.gson.Gson;
 
 public class SimpleProducer {
 
-    private static final String TOPIC = "schematest";
+    private static final String TOPIC = "schematest-avro";
+    private static final String SCHEMA_REGISTRY_URL = "http://40.65.121.104:8081";
+    private static final String KAFKA_URL = "52.175.200.191:31090";
+    private static final Integer RETRIES = 0;
+    private static final String ACKS = "all";
 
     @SuppressWarnings("InfiniteLoopStatement")
     public static void main(final String[] args) {
 
         final Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_URL);
+        props.put(ProducerConfig.ACKS_CONFIG, ACKS);
+        props.put(ProducerConfig.RETRIES_CONFIG, RETRIES);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY_URL);
+
+        // auto register schema with registry
+        props.put("auto.register.schemas", true);
 
         try (KafkaProducer<String, String> producer = new KafkaProducer<>(props)) {
             BadgeService.OpenConnection();
